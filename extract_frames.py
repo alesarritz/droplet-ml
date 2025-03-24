97b95da38b753c8000d6f248c04a2118c31352fe
Abdi Hassan <abdihassan@Abdis-MacBook-Air.local>
2025-03-24 22:18:03 +0100

    Abdi

--- extract_frames.py
+++ extract_frames.py
@@ -37,3 +37,4 @@ def extract_frames(video_path, output_folder, num_frames=120):
 video_path = "20211021_HR13_1_test_converted.mp4"  
 output_folder = "dataset_120_real"  
 extract_frames(video_path, output_folder)
+
